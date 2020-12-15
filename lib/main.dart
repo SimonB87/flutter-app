53aa40113f95f968a9495f69ddd021c1f7d62b7a
SimonB87 <buryans@gmail.com>
2020-12-15 19:55:20 +0100

    adding buttons - icon button with action

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,13 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: RaisedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.mail),
-            label: Text("Mail me!"),
-            color: Colors.amber,
-        ),
+        child: IconButton(
+          onPressed: () {
+            print("You clicked me!");
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
